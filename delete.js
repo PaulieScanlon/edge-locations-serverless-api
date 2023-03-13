a56eafb2ad100495176db008d2d7e9deef2e98b4
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-13 14:19:43 +0000

    feat: add delete function

---
+++ delete.js
@@ -0,0 +1,33 @@
+const { getDB } = require('./pg');
+
+module.exports.handler = async (event) => {
+  const client = await getDB().connect();
+
+  const { id } = JSON.parse(event.body);
+
+  try {
+    await client.query('DELETE FROM locations WHERE id = $1', [id]);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Delete - A Ok!',
+          data: {
+            id: id,
+          },
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Delete - Error!', error: error.message }, null, 2),
+    };
+  } finally {
+    client.release();
+  }
+};

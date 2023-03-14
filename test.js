bdbeda73bdd12602a45cdaf55fa67815b344c44b
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-14 19:10:18 +0000

    test: add test read function

---
+++ test.js
@@ -0,0 +1,32 @@
+const { getDB } = require('./pg');
+const { jsonBeautify } = require('beautify-json');
+
+module.exports.handler = async () => {
+  const client = await getDB().connect();
+
+  try {
+    const response = await client.query('SELECT * from locations');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Read - A Ok!',
+          region: process.env.AWS_REGION,
+          data: {
+            locations: jsonBeautify(response.rows) || [],
+          },
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Read - Error!', region: process.env.AWS_REGION, error: error.message }, null, 2),
+    };
+  } finally {
+    client.release();
+  }
+};
